c86f48c9ef8c95b1dc16afdf5efceffdd4eae312
Rafi Ananta <148605708+Rayakoo@users.noreply.github.com>
2025-03-11 01:31:02 +0700

    fix : bug navigation bar

--- lib/features/presentation/screens/beranda/home_screen.dart
+++ lib/features/presentation/screens/beranda/home_screen.dart
@@ -3,9 +3,26 @@ import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:go_router/go_router.dart';
 import 'package:tes_gradle/features/presentation/router/approutes.dart';
+import 'package:tes_gradle/features/presentation/style/color.dart';
+import 'package:tes_gradle/features/presentation/screens/activity/activity_screen.dart';
+import 'package:tes_gradle/features/presentation/screens/notification/notification_screen.dart';
+import 'package:tes_gradle/features/presentation/screens/profile/profile_screen.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 1;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,8 +51,7 @@ class HomeScreen extends StatelessWidget {
         ],
       ),
       body: FutureBuilder<DocumentSnapshot>(
-        future:
-            FirebaseFirestore.instance.collection('users').doc(user.uid).get(),
+        future: FirebaseFirestore.instance.collection('users').doc(user.uid).get(),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(child: CircularProgressIndicator());
@@ -49,55 +65,58 @@ class HomeScreen extends StatelessWidget {
 
           final userData = snapshot.data!.data() as Map<String, dynamic>;
           final name = userData['name'] ?? 'No name';
-          final nomerIndukKependudukan =
-              userData['nomer_induk_kependudukan'] ?? 'No NIK';
+          final nomerIndukKependudukan = userData['nomer_induk_kependudukan'] ?? 'No NIK';
           final email = user.email ?? 'No email';
 
-          return Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text("Name: $name", style: const TextStyle(fontSize: 18)),
-                Text(
-                  "NIK: $nomerIndukKependudukan",
-                  style: const TextStyle(fontSize: 18),
+          return IndexedStack(
+            index: _selectedIndex,
+            children: [
+              ActivityScreen(),
+              Center(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Name: $name", style: const TextStyle(fontSize: 18)),
+                    Text(
+                      "NIK: $nomerIndukKependudukan",
+                      style: const TextStyle(fontSize: 18),
+                    ),
+                    Text("Email: $email", style: const TextStyle(fontSize: 18)),
+                  ],
                 ),
-                Text("Email: $email", style: const TextStyle(fontSize: 18)),
-              ],
-            ),
+              ),
+              NotificationScreen(),
+              ProfileScreen(),
+            ],
           );
         },
       ),
       bottomNavigationBar: BottomNavigationBar(
-        items: const [
+        showSelectedLabels: true,
+        showUnselectedLabels: false,
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: AppColors.c2a6892,
+        unselectedItemColor: Colors.blueGrey,
+        items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
             icon: Icon(Icons.directions_run),
             label: 'Activity',
           ),
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Homepage'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Homepage',
+          ),
           BottomNavigationBarItem(
             icon: Icon(Icons.notifications),
             label: 'Notification',
           ),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
         ],
-        onTap: (index) {
-          switch (index) {
-            case 0:
-              context.go(AppRoutes.activity);
-              break;
-            case 1:
-              context.go(AppRoutes.homepage);
-              break;
-            case 2:
-              context.go(AppRoutes.notification);
-              break;
-            case 3:
-              context.go(AppRoutes.profile);
-              break;
-          }
-        },
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
       ),
     );
   }
